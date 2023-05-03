b67cc05fd0c0f2ebe205bf82e13bae27f238fcc6
Asif Zaman <eramasif16@gmail.com>
2023-05-04 02:11:27 +0600

    boring-scrapping

--- NLP web-scrapping/NLP fetch-data-URL.py
+++ NLP web-scrapping/NLP fetch-data-URL.py
@@ -1,9 +1,8 @@
 import requests
-from bs4 import BeautifulSoup
+
 
 #fetch data from webpages
 url='https://www.w3schools.com/html/html_styles.asp'
 print("Text data \n\n",requests.get(url).text)
 
-soup=BeautifulSoup()
 
